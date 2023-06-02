5dd630f9f5e04e0a48186f16d9c2357a85b2d32c
Reshav Kumar Gupta <111631321+Mca-reshav@users.noreply.github.com>
2023-06-02 12:46:36 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//Type in typescript
+var a = 10;
+a = 20;
+console.log(a);
+var b = "Abc";
+b = "cbA";
+b = 'xyz';
+console.log(b);
---
+++ app.ts
@@ -0,0 +1,11 @@
+//Type in typescript
+
+let a : Number=10;
+a=20;
+console.log(a)
+
+let b:string="Abc"
+b="cbA"
+b='xyz';
+console.log(b)
+//above both of the cases ts will throw an error of not assign a new value in block level let that is not evident in normal js
